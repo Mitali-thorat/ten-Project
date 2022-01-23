a17ad32acfba76651d14881e385eb0e3d2eb2644
mital <mital@DESKTOP-PQQKCMJ>
2022-01-23 22:30:37 +0530

    ten commit

---
+++ 19-01-2022/src/com/userDefineMethod/Example1.java
@@ -0,0 +1,32 @@
+package com.userDefineMethod;
+
+public class Example1
+
+{
+	//user defined method
+
+	//user defined method definition
+
+	public static void getMessage()//method head/signature
+
+	{
+		//method body/implementation
+		System.out.println("Hello Java.");
+	}	
+
+	//main method is predefined method
+	public static void main(String[] args)
+	{
+		//calling user defined method
+		getMessage();
+	}
+
+
+
+
+
+
+
+}
+
+
